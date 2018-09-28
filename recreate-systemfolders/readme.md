530567deca4bf0559423ad99756febd171bb42ed
Robin van Genderen <robin@kopano.com>
2018-09-28 12:25:21 +0200

    add container class

--- recreate-systemfolders/readme.md
+++ recreate-systemfolders/readme.md
@@ -5,8 +5,8 @@ Script that can recreate system folder*
 
 ## Usage
 ```python
-python recreate-systemfolders --user \<user\> --create <foldername> --systemfolder <systemfoldername**>
+python recreate-systemfolders.py --user \<user\> --create <foldername> --systemfolder <systemfoldername**>
 ```
 
 \* Inbox folder  can't be recreated. 
-\*\* Following systemfolders are available  calendar, contacts, drafts, journal, notes, outbox, sentmail, task, wastebasket
+\*\* Following systemfolders are available  calendar, contacts, drafts, journal, notes, outbox, sentmail, tasks, wastebasket
--- recreate-systemfolders/recreate-systemfolders.py
+++ recreate-systemfolders/recreate-systemfolders.py
@@ -1,14 +1,12 @@
-try:
-    import kopano
-except ImportError:
-    import zarafa as kopano
+import kopano
+from kopano.errors import  *
 import sys
 import binascii
 from MAPI.Util import *
 import uuid
 
 def opt_args():
-    parser = kopano.parser('skpcfm')
+    parser = kopano.parser('skpcfmUP')
     parser.add_option("--user", dest="user", action="store", help="Run script for user")
     parser.add_option("--create", dest="create", action="store", help="create folder")
     parser.add_option("--systemfolder", dest="systemfolder", action="store",
@@ -28,15 +26,32 @@ def getprop(item, myprop):
 def main():
     options, args = opt_args()
     if not options.user:
-        print 'Please use:\n %s --user <username>  ' % (sys.argv[0])
+        print('Please use:\n {} --user <username>  '.format(sys.argv[0]))
         sys.exit(1)
 
     user = kopano.Server(options).user(options.user)
-    print user
-    whitelist = ['calendar', 'contacts', 'drafts', 'journal', 'notes', 'outbox', 'sentmail', 'task',
+    print(user)
+    whitelist = ['calendar', 'contacts', 'drafts', 'journal', 'notes', 'outbox', 'sentmail', 'tasks',
                  'wastebasket']
-    if options.systemfolder in whitelist:
+    name2ipf={
+        'calendar': u'IPF.Appointment',
+        'contacts': u'IPF.Contact',
+        'journal': u'IPF.Journal',
+        'notes': u'IPF.StickyNote',
+        'tasks': u'IPF.Task',
+    }
+    storeid = {"sentmail": PR_IPM_SENTMAIL_ENTRYID,
+               "outbox": PR_IPM_OUTBOX_ENTRYID,
+               "wastebasket": PR_IPM_WASTEBASKET_ENTRYID}
 
+    rootid = {"calendar": PR_IPM_APPOINTMENT_ENTRYID,
+              "contacts": PR_IPM_CONTACT_ENTRYID,
+              "drafts": PR_IPM_DRAFTS_ENTRYID,
+              "journal": PR_IPM_JOURNAL_ENTRYID,
+              "notes": PR_IPM_NOTE_ENTRYID,
+              "tasks": PR_IPM_TASK_ENTRYID}
+
+    if options.systemfolder in whitelist:
         if not options.create:
             oldfolder = getattr(user.store, options.systemfolder)
             oldname = oldfolder.name
@@ -45,58 +60,46 @@ def main():
         # create new sent items box
         if options.create:
             name = options.create
+            print('create folder "{}"'.format(name))
         else:
-            name = uuid.uuid4()
-        try:
-            print 'create tmp folder', name.decode('utf-8')
-            tmp = user.store.subtree.create_folder(name.decode('utf-8'))
-        except MAPIErrorCollision:
-            print '%s already exist' % name.decode('utf-8')
-            sys.exit(1)
+            name = str(uuid.uuid4())
+            print('create tmp folder "{}"'.format(name))
+
+        tmp = user.store.subtree.folder(name, create=True)
 
         if not options.create:
             # move all item to tmp folder
-            print 'copy items from old', options.systemfolder
+            print('copy items from old'.format(options.systemfolder))
             user.store.subtree.copy(oldfolder.items(), tmp)
             # delete old box
 
             if not options.safe:
-                print 'Delete %s folder' % options.systemfolder
+                print('Delete {} folder'.format(options.systemfolder))
                 getattr(user.store, options.systemfolder).delete(getattr(user.store, options.systemfolder))
             else:
                 newname = uuid.uuid4()
-                print 'change the name of the old %s folder to %s' % (options.systemfolder, newname.decode('utf-8'))
-                getattr(user.store, options.systemfolder).mapiobj.SetProps([SPropValue(PR_DISPLAY_NAME, newname.decode('utf-8'))])
-                getattr(user.store, options.systemfolder).mapiobj.SaveChanges(KEEP_OPEN_READWRITE)
+                print('change the name of the old {} folder to {}'.format(options.systemfolder, newname.decode('utf-8')))
+
+                getattr(user.store, options.systemfolder).mapiobj.create_prop(PR_DISPLAY_NAME, newname.decode('utf-8'))
 
 
-            print 'change %s to %s' % (tmp.name, oldname)
-            tmp.mapiobj.SetProps([SPropValue(PR_DISPLAY_NAME, oldname)])
-            tmp.mapiobj.SaveChanges(KEEP_OPEN_READWRITE)
+            print('change {} to {}'.format(tmp.name, oldname))
+            tmp.mapiobj.create_prop(PR_DISPLAY_NAME, oldname)
+            if name2ipf.get(options.systemfolder):
+                bla = tmp.create_prop(PR_CONTAINER_CLASS_W, name2ipf[options.systemfolder])
+                print(tmp.container_class, options.systemfolder)
+
         # promote tmp file to  sytemfolders
-        storeid = {"sentmail": PR_IPM_SENTMAIL_ENTRYID,
-                   "outbox": PR_IPM_OUTBOX_ENTRYID,
-                   "wastebasket": PR_IPM_WASTEBASKET_ENTRYID}
-
-        rootid = {"calendar": PR_IPM_APPOINTMENT_ENTRYID,
-                  "contacts": PR_IPM_CONTACT_ENTRYID,
-                  "drafts": PR_IPM_DRAFTS_ENTRYID,
-                  "journal": PR_IPM_JOURNAL_ENTRYID,
-                  "notes": PR_IPM_NOTE_ENTRYID,
-                  "tasks": PR_IPM_TASK_ENTRYID}
-
-        print 'Promote new folder to be a system folder'
+        print('Promote new folder to be a system folder')
         if options.systemfolder in storeid:
-            user.store.mapiobj.SetProps(
-                [SPropValue(storeid.get(options.systemfolder), binascii.unhexlify(tmp.entryid))])
-            user.store.mapiobj.SaveChanges(KEEP_OPEN_READWRITE)
+            user.store.create_prop(storeid.get(options.systemfolder), binascii.unhexlify(tmp.entryid))
+
         if options.systemfolder in rootid:
-            user.store.root.mapiobj.SetProps(
-                [SPropValue(rootid.get(options.systemfolder), binascii.unhexlify(tmp.entryid))])
-            user.store.root.mapiobj.SaveChanges(KEEP_OPEN_READWRITE)
+            user.store.root.create_prop(rootid.get(options.systemfolder), binascii.unhexlify(tmp.entryid))
 
         if options.safe and not options.create:
-            print '\n\nEntryid of old %s folder :%s\nNew name is:%s' % (oldname.decode('utf-8'), oldid, newname.decode('utf-8'))
+            print('\n\nEntryid of old {} folder :{}\nNew name is:{}'.format(oldname.decode('utf-8'),
+                                                                            oldid, newname.decode('utf-8')))
 
 
 if __name__ == "__main__":
