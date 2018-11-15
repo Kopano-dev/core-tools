34cecee4525dea71d8a9f3bd7cbc4ec2ffef639e
Robin van Genderen <robin@kopano.com>
2018-11-15 14:57:19 +0100

    add fix-calendar script

---
+++ fix-calendar.py
@@ -0,0 +1,33 @@
+import kopano
+from MAPI.Tags import *
+
+def opt_args():
+    parser = kopano.parser('skpcufUP')
+    return parser.parse_args()
+
+def main():
+    options, args = opt_args()
+    server = kopano.Server(options)
+    if not options.users:
+        print('{} -u username'.format(sys.argv[0]))
+    if not options.folders:
+        options.folders = ['Calendar']
+    for user in server.users():
+        for folder in user.folders():
+            for item in folder.items():
+                # start date
+                if not item.get_prop(0x81b60040):
+                    item.create_prop(0x81b60040, item.prop(0x800d0040).value)
+                # end date
+                if not item.get_prop(0x81b70040):
+                    item.create_prop(0x81b70040, item.prop(0x800e0040).value)
+
+                # duration
+                if not item.get_prop(0x80130003):
+                    diff = item.prop(0x800e0040).value - item.prop(0x800d0040).value
+                    item.create_prop(0x80130003, diff.seconds / 60)
+
+
+
+if __name__ == "__main__":
+    main()
