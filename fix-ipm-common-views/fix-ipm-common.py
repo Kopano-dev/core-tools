fd0e5563c23ce3b30b5aa177c0320918156d365d
Robin van Genderen <robin@kopano.com>
2018-12-14 14:24:35 +0100

    add fix-ipm-common script

---
+++ fix-ipm-common-views/fix-ipm-common.py
@@ -0,0 +1,31 @@
+import kopano
+from MAPI.Tags import *
+
+
+def opt_args():
+    parser = kopano.parser('skpcufmUP')
+    parser.add_option("--dry-run", dest="dryrun", action="store_true", help="run the script without executing any actions")
+    return parser.parse_args()
+
+def main():
+    options, args = opt_args()
+    if not options.users:
+        print('pleas use %s -u <username> ' % sys.argv[0])
+        sys.exit(1)
+
+    for user in kopano.Server(options).users():
+        print(user.name)
+        store = user.store
+        common = store.root.folder('IPM_COMMON_VIEWS')
+        subtree = store.root.folder('IPM_SUBTREE')
+        for folder in common.folders():
+            try:
+                print('Copying folder {} to root directory'.format(folder.name))
+            except (UnicodeDecodeError, UnicodeEncodeError):
+                print('Copying folder to root directory')
+            if not options.dryrun:
+                common.move(folder, subtree)
+
+
+if __name__ == "__main__":
+    main()
